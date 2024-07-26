9a40f15ef649f08fb0c229bc262b240683431d7e
cristinavoda <vodacristina7@gmail.com>
2024-07-26 20:00:29 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import router from './router/index.js'; // Asegúrate de que la ruta es correcta
+import router from './router/index.js'; 
 import './style.css';
 
 const app = createApp(App);
 
-app.use(router); // Usa el enrutador en la aplicación
+app.use(router); 
 
 app.mount('#app');
